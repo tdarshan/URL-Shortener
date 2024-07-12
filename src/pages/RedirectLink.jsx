151b6b6487eb4f7f82f8a491bbe6857ce39da545
Darshan Trivedi <trivedidarshan14@gmail.com>
2024-07-12 17:46:34 +0530

    Fix: Testing vercel production

--- src/pages/RedirectLink.jsx
+++ src/pages/RedirectLink.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { storeClicks } from "@/db/apiClicks";
 import { getLongUrl } from "@/db/apiUrls";
 import useFetch from "@/hooks/useFetch";
@@ -23,7 +24,6 @@ const RedirectLink = () => {
     if (!loading && data) {
       fnStats();
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [loading]);
 
   if (loading || loadingStats) {
